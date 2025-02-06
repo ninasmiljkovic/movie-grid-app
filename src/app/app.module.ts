177d1d6d2114b4a994a7e7c631a0edddc441411b
nina.smiljkovic <nina.smiljkovic11@gmail.com>
2025-02-06 22:23:19 +0100

    declares components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieGridComponent } from './components/movie-grid/movie-grid.component';
+import { MovieItemComponent } from './components/movie-item/movie-item.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieGridComponent,
+    MovieItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
