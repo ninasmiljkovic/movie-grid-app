8d404566c72cd78108bc104e23aadd344a0441a9
nina.smiljkovic <nina.smiljkovic11@gmail.com>
2025-02-06 22:21:30 +0100

    adds movie item component

---
+++ src/app/components/movie-item/movie-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Movie } from '../../models/movie.model';
+
+@Component({
+  selector: 'app-movie-item',
+  standalone: false,
+  templateUrl: './movie-item.component.html',
+  styleUrl: './movie-item.component.scss'
+})
+export class MovieItemComponent {
+  @Input() movie!: Movie;
+}
